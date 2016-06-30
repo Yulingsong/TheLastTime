5590741a9b781928f03fafcdb0ef888990bee372
yulingsong <yulingsong5@126.com>
2016-06-30 14:56:13 +0800

    Initial Commit

---
+++ TheLastTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TheLastTime
+//
+//  Created by yulingsong on 16/6/30.
+//  Copyright © 2016年 yulingsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TheLastTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheLastTime
+//
+//  Created by yulingsong on 16/6/30.
+//  Copyright © 2016年 yulingsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
